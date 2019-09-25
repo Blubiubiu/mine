1d9bbd5fa69a85a41c37f73d95e8fcfbc3dd060d
lisiyuan <lisiyuan6@100tal.com>
2019-09-25 17:30:20 +0800

    v1.0.0

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import test from './modules/test';
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	modules: {
-		test
-	}
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
 });
